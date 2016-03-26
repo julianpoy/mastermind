25b0b93299f00465551c5a15fd071ce784841eb2
Charlotte Bryant <charlottemarieb@gmail.com>
2016-03-26 15:21:14 -0700

    Added more game functionality, added div switching

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function popup(){
-  alert("this is not a popup");
+var gameStarted = false;
+
+function setupGame(){
+  gameStarted = true;
+  var gameSetup = document.getElementById('gameSetup');
+  gameSetup.style.display = "none";
+
+  var mainGame = document.getElementById('mainGame');
+  mainGame.style.display = "block";
+}
+
+function changeColor(pinid){
+  var pin = document.getElementById(pinid);
+  pin.style.backgroundColor = "blue";
 }
